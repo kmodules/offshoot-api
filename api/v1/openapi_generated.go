9281ca1ddffdb773af1c706001bb9b94c49ca71a
Tamal Saha <tamal@appscode.com>
2021-03-09 10:14:19 -0800

    Remove deprecated mark on resources (#44)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- api/v1/openapi_generated.go
+++ api/v1/openapi_generated.go
@@ -669,7 +669,7 @@ func schema_kmodulesxyz_offshoot_api_api_v1_PodSpec(ref common.ReferenceCallback
 					},
 					"resources": {
 						SchemaProps: spec.SchemaProps{
-							Description: "Compute Resources required by the sidecar container. Deprecated",
+							Description: "Compute Resources required by the sidecar container.",
 							Default:     map[string]interface{}{},
 							Ref:         ref("k8s.io/api/core/v1.ResourceRequirements"),
 						},
